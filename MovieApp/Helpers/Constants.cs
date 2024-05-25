dfdc603b6b214c242b799c7a22c086ae27553b54
Parox35 <antoine.esnault4@gmail.com>
2024-05-25 19:20:04 +0200

    Add helper folder

---
+++ MovieApp/Helpers/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Helpers
+{
+    public class Constants
+    {
+        public static string DbConnectionString = ""; //Add the database connection here
+
+    }
+}
