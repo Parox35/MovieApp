8b1fc4ea5cf444f2074cef04e58b94bc60c0c70c
Parox35 <antoine.esnault4@gmail.com>
2024-05-25 15:22:42 +0200

    Add views

--- MovieApp/Views/Details.xaml.cs
+++ MovieApp/Views/Details.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,19 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using MovieApp.Views;
-
-namespace MovieApp
+namespace MovieApp.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour Details.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Details : Page
     {
-        public MainWindow()
+        public Details()
         {
             InitializeComponent();
-            MainFrame.Navigate(new Home(MainFrame));
         }
     }
 }
--- MovieApp/Views/Home.xaml.cs
+++ MovieApp/Views/Home.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,19 +13,18 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using MovieApp.Views;
-
-namespace MovieApp
+namespace MovieApp.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour Home.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Home : Page
     {
-        public MainWindow()
+        private Frame mainFrame;
+        public Home(Frame mainFrame)
         {
             InitializeComponent();
-            MainFrame.Navigate(new Home(MainFrame));
+            this.mainFrame = mainFrame;
         }
     }
 }
