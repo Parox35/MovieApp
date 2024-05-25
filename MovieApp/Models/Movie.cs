60fad3840ef9746f2269d23b9e334925f3d53b0c
Parox35 <antoine.esnault4@gmail.com>
2024-05-25 15:23:01 +0200

    Add models folder

---
+++ MovieApp/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Duration { get; set; }
+        public string Summary { get; set; }
+    }
+}
